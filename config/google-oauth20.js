8962046b5cd34bec3283263a23600f573add1d97
Ratul Anjum <mdratulanjum@gmail.com>
2024-03-12 18:06:57 +0600

    callback

--- config/google-oauth20.js
+++ config/google-oauth20.js
@@ -6,7 +6,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8000/auth/google/callback"
+    callbackURL: "https://bita1.vercel.app/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, cb) => {
     try {
