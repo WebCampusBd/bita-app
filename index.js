59c73e6b9f9a36e8e29701162a41cf9619e7d6d5
Ratul Anjum <mdratulanjum@gmail.com>
2024-03-13 11:43:18 +0600

    clientConnectAdd

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { app } = require("./app");
 const { dbConnect } = require("./config/database");
 const port = process.env.PORT;
 
+// await client.connect(); 
+// await client.db("admin").command({ ping: 1 });
 
 app.listen(port, async ()=>{
     console.log(`Server is runnig at http://localhost:${port}`);
